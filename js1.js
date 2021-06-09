14d42b351e522c326c11c35eb96a15db7985029b
Lucas Curty <74004642+LucasCurty@users.noreply.github.com>
2021-06-09 19:58:08 -0300

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,7 +1,7 @@
 let nome = prompt("Qual seu nome?");
 let res = document.getElementById('res')
 alert(`Olá ${nome}, Aviso! NÃO interaja como o botão!`)
-let conta = 28;
+let conta = 0;
 let bum = document.getElementById('img')
 let som = document.getElementById('aud')
 let corpo = document.getElementById('corpo')
